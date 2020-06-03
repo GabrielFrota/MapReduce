55b8e80432ce90bebc322620a91a05841db72010
gabriel@gabriel-System-Product-Name <gabriel.frota@yoopay.com.br>
2020-06-03 14:10:53 -0300

    a

--- src/core/Worker.java
+++ src/core/Worker.java
@@ -1,5 +1,6 @@
 package core;
 
+import java.net.Socket;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.Scanner;
@@ -9,15 +10,19 @@ public class Worker implements Callable<Integer> {
 		
 	@Override
 	public Integer call() throws Exception {
-		System.setProperty("java.rmi.server.hostname", "192.168.25.7");
+		Socket s = new Socket("www.google.com", 80);
+		System.setProperty("java.rmi.server.hostname", s.getLocalAddress().getHostAddress());
+		s.close();
+		
 		WorkerRemoteImpl impl = new WorkerRemoteImpl();
 		Registry reg = LocateRegistry.createRegistry(1099);
 		reg.bind(WorkerRemote.LOOKUPNAME, impl);	
 		System.out.println("RMI Registry binded to port 1099 exporting WorkerRemote interface.\n"
 				+ "Type \"quit\" to stop the server and close the JVM.");
+		
 		Scanner scan = new Scanner(System.in);
 		while (!scan.nextLine().equals("quit"));
-		scan.close();
+		scan.close();		
 		return 0;
 	}
 	
