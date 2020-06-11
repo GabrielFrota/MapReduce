f331b4ba85877fb1293c4671dc5c6e25e31cc5a7
gabriel@gabriel-System-Product-Name <gabriel.frota@yoopay.com.br>
2020-06-11 17:02:27 -0300

    m

--- src/core/Worker.java
+++ src/core/Worker.java
@@ -20,7 +20,7 @@ public class Worker implements Callable<Integer> {
     private String chunk;  
     @Override public int sendMapChunk(String c) throws RemoteException {
       chunk = c;
-      return c.length();
+      return chunk.length();
     }
 
   }
@@ -29,12 +29,12 @@ public class Worker implements Callable<Integer> {
   public Integer call() throws Exception {
     try (var sock = new Socket("www.google.com", 80)) {
       System.setProperty("java.rmi.server.hostname", sock.getLocalAddress().getHostAddress());
-      System.out.println(sock.getLocalAddress().getHostAddress());
     }
     var impl = new WorkerRemoteImpl();
     var reg = LocateRegistry.createRegistry(1099);
+    var ip = System.getProperty("java.rmi.server.hostname");
     reg.bind(WorkerRemote.LOOKUP_NAME, impl);
-    System.out.println("RMI Registry binded to port 1099 exporting WorkerRemote interface.\n"
+    System.out.println("RMI Registry is binded to address " + ip + ":1099 exporting WorkerRemote interface.\n"
         + "Type \"quit\" to stop the server and close the JVM.");
 
     try (var scan = new Scanner(System.in)) {
