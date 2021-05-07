4c3d58805a2f3cc8572269372a83de80921bbaee
gabriel@gabriel-System-Product-Name <gabriel.frota@yoopay.com.br>
2021-05-07 01:55:12 -0300

    a

--- src/test/WordCount.java
+++ src/test/WordCount.java
@@ -1,6 +1,8 @@
 package test;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.TreeMap;
@@ -8,23 +10,23 @@ import java.util.TreeMap;
 public class WordCount {
 
   public static void main(String[] args) throws IOException {
-    var map = new TreeMap<Integer, Integer>();
+    //System.setOut(new PrintStream(new File("count.txt")));
+    var map = new TreeMap<String, Integer>();
     var lines = Files.readAllLines(Paths.get(args[0]));
     for (var l : lines) {
       var strs = l.split(",");
       for (var s : strs) {
-        var i = Integer.parseInt(s);
-        if (map.containsKey(i))
-          map.put(i, map.get(i) + 1);
+        if (map.containsKey(s))
+          map.put(s, map.get(s) + 1);
         else
-          map.put(i, 1);
+          map.put(s, 1);
       }
     }
     for (var en : map.entrySet()) {
       System.out.println(en.getKey() + "\t" + en.getValue());
     }
     
-//    int key = 0;
+//    String key = null;
 //    int value = 0;
 //    for (var en : map.entrySet()) {
 //      if (en.getValue() > value) {
